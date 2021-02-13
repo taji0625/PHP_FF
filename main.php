57d3424a63403e6e46b52da0c5fdd9eb19b935ea
Takuma Tajikawa <t.g.k.w.t.k.m@gmail.com>
2021-02-13 10:49:52 +0900

    HPの表示を実装

--- main.php
+++ main.php
@@ -20,11 +20,15 @@ $turn = 1;
 
 while ($tiida->getHitPoint() > 0 && $goblin->getHitPoint() > 0) {
   echo "*** $turn ターン目 ***\n\n"; 
-  echo $tiida->getName() . " ： " . $tiida->getHitPoint() . "/" . $tiida::MAX_HITPOINT . "\n";
-  echo $goblin->getName() . " ： " . $goblin->getHitPoint() . "/" . $goblin::MAX_HITPOINT . "\n";
+  foreach ($members as $member) {
+    echo $member->getName() . " : " . $member->getHitPoint() . "/" . $member::MAX_HITPOINT . "\n";
+  }
+  echo "\n";
+  foreach ($enemies as $enemy) {
+    echo $enemy->getName() . " : " . $enemy->getHitPoint() . "/" . $enemy::MAX_HITPOINT . "\n";
+  }
   echo "\n";
 
-  // 攻撃
   $tiida->doAttack($goblin);
   echo "\n";
   $goblin->doAttack($tiida);
