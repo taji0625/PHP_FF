5a4721ab71ee02cf9f7989ef54b2d39f96cff71a
Takuma Tajikawa <t.g.k.w.t.k.m@gmail.com>
2021-02-13 09:05:37 +0900

    WhiteMageクラスの宣言とプロパティの定義

--- classes/BlackMage.php
+++ classes/BlackMage.php
@@ -6,4 +6,18 @@ class BlackMage extends Human
   private $hitPoint = 80;
   private $attackPoint = 10;
   private $intelligence = 30;
+
+
+  public function doAttack($enemy)
+  {
+    if(rand(1,2) === 1) {
+      echo "『" .$this->getName() . "』のスキルが発動した！\n";
+      echo "『ファイア』！！\n";
+      echo $enemy->getName() . " に " . $this->intelligence * 1.5 . " のダメージ！\n";
+      $enemy->tookDamage($this->intelligence * 1.5);
+    } else {
+      parent::doAttack($enemy);
+    }
+    return true;
+  }
 }
---
+++ classes/WhiteMage.php
@@ -0,0 +1,9 @@
+<?php
+
+class WhiteMage extends Human
+{
+    const MAX_HITPOINT = 80;
+    private $hitPoint = 80;
+    private $attackPoint = 10;
+    private $intelligence = 30;
+}
