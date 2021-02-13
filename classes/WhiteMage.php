36e3be9cb4edefbd35710e3dd98e56bbf4e1e5be
Takuma Tajikawa <t.g.k.w.t.k.m@gmail.com>
2021-02-13 09:10:46 +0900

    doAttackWhiteMageメソッドを定義

--- classes/WhiteMage.php
+++ classes/WhiteMage.php
@@ -6,4 +6,17 @@ class WhiteMage extends Human
     private $hitPoint = 80;
     private $attackPoint = 10;
     private $intelligence = 30;
+
+  public function doAttackWhiteMage($enemy, $human)
+  {
+    if(rand(1,2) === 1) {
+      echo "『" .$this->getName() . "』のスキルが発動した！\n";
+      echo "『ケアル』！！\n";
+      echo $human->getName() . " のHPを " . $this->intelligence * 1.5 . " 回復！\n";
+      $human->recoveryDamage($this->intelligence * 1.5, $human);
+    } else {
+      parent::doAttack($enemy);
+    }
+    return true;
+  }
 }
